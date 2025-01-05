57471abe5b069c511b02ea9225d42a710d0438c1
MULIPUJITHA <136082749+MULIPUJITHA@users.noreply.github.com>
2025-01-05 23:07:42 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+
+const App = () => (
+    <div>
+        <Header />
+        <Home />
+        <Footer />
+    </div>
+);
+
+export default App;
