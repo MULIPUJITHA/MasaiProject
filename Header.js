854ae7dd71a5ac348a58dab477059ca2d78c8dc0
MULIPUJITHA <136082749+MULIPUJITHA@users.noreply.github.com>
2025-01-05 23:08:07 +0530

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => (
+    <header>
+        <h1>FashionHub</h1>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/products">Products</a></li>
+                <li><a href="/cart">Cart</a></li>
+            </ul>
+        </nav>
+    </header>
+);
+
+export default Header;
